136d54815aeda62ee6adb53e9410bd4cbce62a0e
Aditi Agrawal <agrawal.aditi@razorpay.com>
2025-08-25 15:38:58 +0530

    Day 3

---
+++ httpclient/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
